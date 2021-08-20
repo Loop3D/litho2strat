42498ed40c54e687c29dfe2b93b10cb4ca375679
vogarko <vogarko@gmail.com>
2021-08-20 13:28:31 +0800

    Made functions for getting strata distribution, and route scrores.

--- litho2strat.py
+++ litho2strat.py
@@ -495,26 +495,29 @@ def print_foreign_lithos(route):
         unit_index += 1
 
 #=============================================================================
-def plot_unit_probabilities(all_routes, drillsample_data, num_units):
+def get_strat_distr(all_routes, num_units):
     '''
-    Generate a plot with probability of occurence for each unit.
+    Returns the distribution of unit presence at every depth.
     '''
-    if (len(all_routes) == 0):
-        return
-
     num_rows = len(all_routes[0].path)
     strat_distr = np.zeros((num_rows, num_units))
 
-    # Building the distribution of unit presence at every depth.
     for route in all_routes:
         for row in range(num_rows):
             unit_index = route.path[row]
             strat_distr[row, unit_index] += 1
     # Normalize.
     strat_distr = strat_distr / float(len(all_routes))
+    return strat_distr
 
-    #------------------------------------------
-    # Plot distribution of the route scores (based on path probability).
+#=============================================================================
+def get_route_scores(all_routes, strat_distr):
+    '''
+    Returns the route scores (based on path probability).
+    Needs strat_distr returned by get_strat_distr().
+    '''
+    num_rows = len(all_routes[0].path)
+
     route_scores = np.zeros(len(all_routes), dtype=float)
     route_index = 0
     for route in all_routes:
@@ -523,6 +526,22 @@ def plot_unit_probabilities(all_routes, drillsample_data, num_units):
             route_scores[route_index] += strat_distr[row, unit_index]
         route_scores[route_index] = route_scores[route_index] / float(num_rows)
         route_index += 1
+    return route_scores
+
+#=============================================================================
+def plot_unit_probabilities(all_routes, drillsample_data, num_units):
+    '''
+    Generate a plot with probability of occurence for each unit.
+    '''
+    if (len(all_routes) == 0):
+        return
+
+    # Building the distribution of unit presence at every depth.
+    strat_distr = get_strat_distr(all_routes, num_units)
+
+    #------------------------------------------
+    # Plot distribution of the route scores (based on path probability).
+    route_scores = get_route_scores(all_routes, strat_distr)
 
     title_params = 'Max foreign lithos = ' + str(max_num_foreign_lithos) + ', max returns = ' + str(max_num_returns)
 
@@ -545,6 +564,8 @@ def plot_unit_probabilities(all_routes, drillsample_data, num_units):
     pl.rcParams["figure.figsize"] = (12.8, 9.6) # Default size = (6.4, 4.8)
     fig, axs = pl.subplots(num_units, sharey=True)
     fig.suptitle('Probability for each unit. ' + title_params)
+
+    num_rows = len(all_routes[0].path)
 
     # Using the "From" column.
     x_data = [float(d[0]) for d in drillsample_data[0:num_rows]]
