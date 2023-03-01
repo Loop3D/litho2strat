4921f823818324aa33242424f891ba22008b19ca
Vitaliy Ogarko <vogarko@gmail.com>
2023-02-28 21:06:45 -0800

    Display unit name in solution logs.

--- solution_utils.py
+++ solution_utils.py
@@ -254,6 +254,10 @@ def draw_solution_logs(strat_solution, display_plot, type, unit_colors_filename,
             patches.append(Rectangle((x1, y1), dx, dy))
             color_list.append(color)
 
+            display_unit_name = True
+            if (display_unit_name):
+                pl.text(x1, y1, strat_solution.unit_names[route.path[row]])
+
     # Define patches collection with colormap.
     patches_cmap = ListedColormap(color_list)
     patches_collection = PatchCollection(patches, cmap=patches_cmap)
