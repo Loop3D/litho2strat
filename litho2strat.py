9905acda1cfd5e93d431e94ee8fd92001162752c
Vitaliy Ogarko <vogarko@gmail.com>
2023-03-01 21:22:03 -0800

    Added default parameter values.

--- litho2strat.py
+++ litho2strat.py
@@ -57,36 +57,36 @@ def main():
     spar = StrataSolverParameters()
 
     # Unit contact topology constraints (extracted from map data).
-    spar.add_topology_constraints = config.getboolean(section, 'add_topology_constraints')
+    spar.add_topology_constraints = config.getboolean(section, 'add_topology_constraints', fallback = True)
 
     # 'Age alignment' constraints: the maximum number of times the age direction can flip.
-    spar.max_num_age_flips = config.getint(section, 'max_num_age_flips')
+    spar.max_num_age_flips = config.getint(section, 'max_num_age_flips', fallback = 2)
 
     # 'Returning to the same unit' constraints.
-    spar.max_num_returns_per_unit = config.getint(section, 'max_num_returns_per_unit')
+    spar.max_num_returns_per_unit = config.getint(section, 'max_num_returns_per_unit', fallback = 1)
 
     # The maximum number of unit contacts inside the same litholgy sequence.
-    spar.max_num_unit_contacts_inside_litho = config.getint(section, 'max_num_unit_contacts_inside_litho')
+    spar.max_num_unit_contacts_inside_litho = config.getint(section, 'max_num_unit_contacts_inside_litho', fallback = 0)
 
     # Use the single closest unit for the top (first) lithology.
-    spar.single_top_unit = config.getboolean(section, 'single_top_unit')
+    spar.single_top_unit = config.getboolean(section, 'single_top_unit', fallback = True)
 
     #---------------------------------
     section = 'DataPreprocessing'
     print(config.items(section))
 
     # The number of nearest units (for distance constraints).
-    number_nearest_units = config.getint(section, 'number_nearest_units')
+    number_nearest_units = config.getint(section, 'number_nearest_units', fallback = 3)
 
     # Minimum score for drillhole lithologies to use them.
-    min_drillhole_litho_score = config.getint(section, 'min_drillhole_litho_score')
+    min_drillhole_litho_score = config.getint(section, 'min_drillhole_litho_score', fallback = 80)
 
     # Group drillhole lithology sequence.
     # Note: use this for max_num_unit_contacts_inside_litho > 0 to avoid the solution number to blow.
-    group_drillhole_lithos = config.getboolean(section, 'group_drillhole_lithos')
+    group_drillhole_lithos = config.getboolean(section, 'group_drillhole_lithos', fallback = False)
 
     # The cover ration threshold (relative length) for removing the cover.
-    cover_ratio_threshold = config.getfloat(section, 'cover_ratio_threshold')
+    cover_ratio_threshold = config.getfloat(section, 'cover_ratio_threshold', fallback = 0.65)
 
     #---------------------------------
     section = 'CollarIDs'
